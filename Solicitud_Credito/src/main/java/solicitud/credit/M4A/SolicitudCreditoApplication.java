9cb38653f9bc17d4997a946cb1f235f823c200b0
MEJI-469 <mauriciomeji469@gmail.com>
2024-05-29 21:02:38 -0500

    .

---
+++ Solicitud_Credito/src/main/java/solicitud/credit/M4A/SolicitudCreditoApplication.java
@@ -0,0 +1,13 @@
+package solicitud.credit.M4A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudCreditoApplication.class, args);
+	}
+
+}
---
+++ Solicitud_Credito/src/test/java/solicitud/credit/M4A/SolicitudCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package solicitud.credit.M4A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

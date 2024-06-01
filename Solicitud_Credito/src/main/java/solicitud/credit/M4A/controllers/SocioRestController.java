54c62af4f438448b0b9fdee8e96fbb84051f7036
derekvergara <112039686+derekvergara@users.noreply.github.com>
2024-06-01 16:38:36 -0500

    cambios

--- Solicitud_Credito/src/main/java/solicitud/credit/M4A/controllers/SocioRestController.java
+++ Solicitud_Credito/src/main/java/solicitud/credit/M4A/controllers/SocioRestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import solicitud.credit.M4A.models.entity.Socio;
 import solicitud.credit.M4A.models.services.ISocioService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -17,12 +19,11 @@ public class SocioRestController {
 
 	@Autowired
 	public ISocioService socioService;
-	
+	//Listas
 	@GetMapping("/socios")
 	public List<Socio> index() {
 		return socioService.findAll();
-	}
-	
+	}	
 	// url para testear los datos
 	//http://localhost:8080/api/socios
 	

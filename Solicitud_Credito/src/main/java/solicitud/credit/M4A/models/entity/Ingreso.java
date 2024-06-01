87df15e30370af04f187746edb43d2a4ee096232
derekvergara <112039686+derekvergara@users.noreply.github.com>
2024-06-01 18:54:29 -0500

    clae ingreso

--- Solicitud_Credito/src/main/java/solicitud/credit/M4A/models/entity/Ingreso.java
+++ Solicitud_Credito/src/main/java/solicitud/credit/M4A/models/entity/Ingreso.java
@@ -10,91 +10,112 @@ import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="ingresos", uniqueConstraints = {@UniqueConstraint(columnNames = {"CedSocioIngr"})})
+@Table(name="ingresos", uniqueConstraints = {@UniqueConstraint(columnNames = {"ced_socio_ingr"})})
 public class Ingreso implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long Id;
+	private Long id;
 	
-	private String CedSocioIngr;
-	private Double PorIntGanadIngr;
-	private Double PorPenRecIngr;
-	private Double PorAlquiVehiIngr;
-	private Double PorArriReciIngr;
-	private Double GananciaGanaIngr;
-	private Double GananciaAgriIngr;
-	private Double GananciaComerIngr;
-	private Double SueldConyugIngr;
-	private Double SueldSocioIngr;
-	
+	private String ced_socio_ingr;
+	private Double por_int_ganad_ingr;
+	private Double por_pen_rec_ingr;
+	private Double por_alqui_vehi_ingr;
+	private Double por_arri_reci_ingr;
+	private Double ganancia_gana_ingr;
+	private Double ganancia_agri_ingr;
+	private Double ganancia_comer_ingr;
+	private Double sueld_conyug_ingr;
+	private Double sueld_socio_ingr;
+		
 	public Long getId() {
-		return Id;
+		return id;
 	}
+
 	public void setId(Long id) {
-		Id = id;
+		this.id = id;
 	}
-	public String getCedSocioIngr() {
-		return CedSocioIngr;
+
+	public String getCed_socio_ingr() {
+		return ced_socio_ingr;
 	}
-	public void setCedSocioIngr(String cedSocioIngr) {
-		CedSocioIngr = cedSocioIngr;
+
+	public void setCed_socio_ingr(String ced_socio_ingr) {
+		this.ced_socio_ingr = ced_socio_ingr;
 	}
-	public Double getPorIntGanadIngr() {
-		return PorIntGanadIngr;
+
+	public Double getPor_int_ganad_ingr() {
+		return por_int_ganad_ingr;
 	}
-	public void setPorIntGanadIngr(Double porIntGanadIngr) {
-		PorIntGanadIngr = porIntGanadIngr;
+
+	public void setPor_int_ganad_ingr(Double por_int_ganad_ingr) {
+		this.por_int_ganad_ingr = por_int_ganad_ingr;
 	}
-	public Double getPorPenRecIngr() {
-		return PorPenRecIngr;
+
+	public Double getPor_pen_rec_ingr() {
+		return por_pen_rec_ingr;
 	}
-	public void setPorPenRecIngr(Double porPenRecIngr) {
-		PorPenRecIngr = porPenRecIngr;
+
+	public void setPor_pen_rec_ingr(Double por_pen_rec_ingr) {
+		this.por_pen_rec_ingr = por_pen_rec_ingr;
 	}
-	public Double getPorAlquiVehiIngr() {
-		return PorAlquiVehiIngr;
+
+	public Double getPor_alqui_vehi_ingr() {
+		return por_alqui_vehi_ingr;
 	}
-	public void setPorAlquiVehiIngr(Double porAlquiVehiIngr) {
-		PorAlquiVehiIngr = porAlquiVehiIngr;
+
+	public void setPor_alqui_vehi_ingr(Double por_alqui_vehi_ingr) {
+		this.por_alqui_vehi_ingr = por_alqui_vehi_ingr;
 	}
-	public Double getPorArriReciIngr() {
-		return PorArriReciIngr;
+
+	public Double getPor_arri_reci_ingr() {
+		return por_arri_reci_ingr;
 	}
-	public void setPorArriReciIngr(Double porArriReciIngr) {
-		PorArriReciIngr = porArriReciIngr;
+
+	public void setPor_arri_reci_ingr(Double por_arri_reci_ingr) {
+		this.por_arri_reci_ingr = por_arri_reci_ingr;
 	}
-	public Double getGananciaGanaIngr() {
-		return GananciaGanaIngr;
+
+	public Double getGanancia_gana_ingr() {
+		return ganancia_gana_ingr;
 	}
-	public void setGananciaGanaIngr(Double gananciaGanaIngr) {
-		GananciaGanaIngr = gananciaGanaIngr;
+
+	public void setGanancia_gana_ingr(Double ganancia_gana_ingr) {
+		this.ganancia_gana_ingr = ganancia_gana_ingr;
 	}
-	public Double getGananciaAgriIngr() {
-		return GananciaAgriIngr;
+
+	public Double getGanancia_agri_ingr() {
+		return ganancia_agri_ingr;
 	}
-	public void setGananciaAgriIngr(Double gananciaAgriIngr) {
-		GananciaAgriIngr = gananciaAgriIngr;
+
+	public void setGanancia_agri_ingr(Double ganancia_agri_ingr) {
+		this.ganancia_agri_ingr = ganancia_agri_ingr;
 	}
-	public Double getGananciaComerIngr() {
-		return GananciaComerIngr;
+
+	public Double getGanancia_comer_ingr() {
+		return ganancia_comer_ingr;
 	}
-	public void setGananciaComerIngr(Double gananciaComerIngr) {
-		GananciaComerIngr = gananciaComerIngr;
+
+	public void setGanancia_comer_ingr(Double ganancia_comer_ingr) {
+		this.ganancia_comer_ingr = ganancia_comer_ingr;
 	}
-	public Double getSueldConyugIngr() {
-		return SueldConyugIngr;
+
+	public Double getSueld_conyug_ingr() {
+		return sueld_conyug_ingr;
 	}
-	public void setSueldConyugIngr(Double sueldConyugIngr) {
-		SueldConyugIngr = sueldConyugIngr;
+
+	public void setSueld_conyug_ingr(Double sueld_conyug_ingr) {
+		this.sueld_conyug_ingr = sueld_conyug_ingr;
 	}
-	public Double getSueldSocioIngr() {
-		return SueldSocioIngr;
+
+	public Double getSueld_socio_ingr() {
+		return sueld_socio_ingr;
 	}
-	public void setSueldSocioIngr(Double sueldSocioIngr) {
-		SueldSocioIngr = sueldSocioIngr;
+
+	public void setSueld_socio_ingr(Double sueld_socio_ingr) {
+		this.sueld_socio_ingr = sueld_socio_ingr;
 	}
-	
+
 	/**
 	 * 
 	 */

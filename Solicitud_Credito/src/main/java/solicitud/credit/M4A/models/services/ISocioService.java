dde7e3b7d5f97cfaa7cd3a3a222f52de7cbbbd2e
derekvergara <112039686+derekvergara@users.noreply.github.com>
2024-06-01 19:21:26 -0500

    metodos creados

--- Solicitud_Credito/src/main/java/solicitud/credit/M4A/models/services/ISocioService.java
+++ Solicitud_Credito/src/main/java/solicitud/credit/M4A/models/services/ISocioService.java
@@ -7,5 +7,8 @@ import solicitud.credit.M4A.models.entity.Socio;
 public interface ISocioService {
 
 	public List<Socio> findAll();
+	public Socio findById(String id);
+	public Socio save(Socio socio);
+	public void delete(String id);
 	
 }

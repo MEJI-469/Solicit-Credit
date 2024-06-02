e6dd9e70c9b93f15746914bc3d991ece299e7154
MEJI-469 <mauriciomeji469@gmail.com>
2024-06-02 18:11:51 -0500

    daños dañados de derek

--- Solicitud_Credito/src/main/java/solicitud/credit/M4A/controllers/SocioRestController.java
+++ Solicitud_Credito/src/main/java/solicitud/credit/M4A/controllers/SocioRestController.java
@@ -7,29 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ista.cursoM4A.models.entity.Cliente;
 import solicitud.credit.M4A.models.entity.Socio;
 import solicitud.credit.M4A.models.services.ISocioService;
-<<<<<<< HEAD
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-=======
->>>>>>> 7cdd2395e20c33a4c1a10b91d145fb0dc17db86b
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -43,31 +30,11 @@ public class SocioRestController {
 	@GetMapping("/socios")
 	public List<Socio> index() {
 		return socioService.findAll();
-	}	
-	
-<<<<<<< HEAD
-	//Busqueda por id
-		@GetMapping("/socios/{id}")
-		public Socio  show(@PathVariable String id){
-			return socioService.findById(id);
-		}
-		
-		//insertar o actualizar en la base de datos
-		@PostMapping("/socios")
-		@ResponseStatus(HttpStatus.CREATED)
-		public Socio create(@RequestBody Socio socio) {
-			return socioService.save(socio);
-		}
-		//Eliminar
-		@DeleteMapping("/socios/{id}")
-		@ResponseStatus(HttpStatus.NO_CONTENT)//mensaje actualizacion de los datos
-		public void delete(@PathVariable String id) {
-			socioService.delete(id);
-		}
+	}
 	
 	// url para testear los datos
 	//http://localhost:8080/api/socios
-=======
+
 	//Buscar socio
 	@GetMapping("/socios/{cedula}")
 	public Socio show(@PathVariable String cedula) {
@@ -116,6 +83,5 @@ public class SocioRestController {
 		// TODO Auto-generated method stub
 		socioService.delete(cedula);
 	}
->>>>>>> 7cdd2395e20c33a4c1a10b91d145fb0dc17db86b
 	
 }
